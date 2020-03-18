ea85567d8daeb70f8a2d3bf7d47ee189d7e5be53
eeden01 <mmvaneeden@gmail.com>
2020-03-18 17:10:21 +0100

    bug fixes

--- backend/controller.js
+++ backend/controller.js
@@ -1,42 +1,5 @@
-<<<<<<< HEAD
-// const Organization = require('./models/model-organization');
-// const User = require('./models/model-user');
 const Step = require('./models/model-steps');
-=======
-const User = require('./models/model-user');
-const Steps = require('./models/model-steps');
->>>>>>> e53d5030fa7d4f823028d3fd76d6a8de6020b9ba
 const Journey = require('./models/model-journey');
-// const mongoose = require('mongoose');
-
-<<<<<<< HEAD
-// exports.loginOrg = async (req, res) => {
-//   try {    //sort out object user schema
-//     const org = await Organization.findOne({ name: req.body.organization, username: req.body.username, password: req.body.password })
-//     console.log('org', org);
-//     if (org === null) {
-//       res.status(404).send('Something broke!')
-//     } else {
-//       res.status(201);
-//       res.json(org);
-//     }
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).send();
-//   }
-// }
-
-// exports.registerOrg = async (req, res) => {
-//   try {
-//     console.log('req', req.body);
-//     const user = await Organization.create({ name: req.body.organization });
-//     res.status(201);
-//     res.json(user);
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).send();
-//   }
-// }
 
 exports.postStep = async (req, res) => {
   try {
@@ -61,20 +24,6 @@ exports.postJourney = async (req, res) => {
     const journey = await Journey.create({ title: req.body.journey, steps: [] });
     res.status(201);
     res.json(journey);
-=======
-exports.postStep = async (req, res) => {
-  try {
-    const journey = await Journey.Model.findById(req.params.id);
-    journey.steps.push(
-      {
-        title: req.body.title,
-        emotion: req.body.emotion,
-        score: req.body.score
-      });
-    await journey.save();
-    res.json(journey);
-    res.status(201);
->>>>>>> e53d5030fa7d4f823028d3fd76d6a8de6020b9ba
   } catch (error) {
     console.log(error);
     res.status(500).send();
@@ -114,12 +63,7 @@ exports.updateStep = async (req, res) => {
       }
     )
     res.status(201);
-<<<<<<< HEAD
-    res.json();
-=======
     res.json(journey.steps);
->>>>>>> e53d5030fa7d4f823028d3fd76d6a8de6020b9ba
-
   } catch (error) {
     console.log(error)
     res.status(500).send();
