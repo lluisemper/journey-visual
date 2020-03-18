e7b4ffbb8bc88718e2884dadf406e165d0bab557
eeden01 <mmvaneeden@gmail.com>
2020-03-18 21:23:04 +0100

    style changes

--- client/src/components/Step/Step.js
+++ client/src/components/Step/Step.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Step.css';
 import ApiClient from '../../ApiClient';
 
@@ -6,35 +6,42 @@ import ApiClient from '../../ApiClient';
 const Step = ({ step, addStep }) => {
 
   const [initialState, setinitialState] = useState({
-    title:'',
-    emotion:'',
-    score:''
+    title: '',
+    emotion: '',
+    score: ''
   })
 
   const submitHandler = (e) => {
     e.preventDefault();
     addStep(step._id, initialState)
   }
-  
-    return (
-      <div className='Step'>
-      {step.title ? <div>
+
+  return (
+    <div className='Step'>
+      {step.title ? <div className="step-active">
         <h1>{step.title}</h1>
         <h1>{step.emotion}</h1>
         <h1>{step.score}</h1>
       </div> :
-      <form className="stepForm" onSubmit={submitHandler}>
-        <h4>Title:</h4>
-        <input type='text'onChange={(e) => setinitialState({...initialState, title: e.target.value})}/>
-        <h4>Emotion</h4>
-        <input type='text'onChange={(e) => setinitialState({...initialState, emotion: e.target.value})}/>
-        <h4>Score</h4>
-        <input type='text'onChange={(e) => setinitialState({...initialState, score: e.target.value})}/>
-        <input type='submit'/>
-      </form>} 
-     </div>
-    )
-  
+        <form className="stepForm" onSubmit={submitHandler}>
+          <div className="wrapper">
+            <h4>Title:</h4>
+            <input type='text' onChange={(e) => setinitialState({ ...initialState, title: e.target.value })} />
+          </div>
+
+          <div className="wrapper">
+            <h4>Emotion</h4>
+            <input type='text' onChange={(e) => setinitialState({ ...initialState, emotion: e.target.value })} />
+          </div>
+          <div className="wrapper">
+            <h4>Score</h4>
+            <input type='text' onChange={(e) => setinitialState({ ...initialState, score: e.target.value })} />
+          </div>
+          <input type='submit' />
+        </form>}
+    </div>
+  )
+
 }
 
 export default Step
