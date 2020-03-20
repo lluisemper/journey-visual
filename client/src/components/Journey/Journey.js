1f64adb3e2d1dc74b1d9a9a98bd8b5fae051e0db
eeden01 <mmvaneeden@gmail.com>
2020-03-20 19:24:04 +0100

    styling

--- client/src/components/Journey/Journey.js
+++ client/src/components/Journey/Journey.js
@@ -1,16 +1,59 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 import './Journey.css';
 
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+    margin: 20
+  },
+  media: {
+    height: 140,
+  },
+});
+
 const Journey = ({ journey, setCurrentJourney }) => {
+  const classes = useStyles();
+
   return (
-    <button className='Journey' onClick={() => {
+    <Card className={classes.root} onClick={() => {
       setCurrentJourney(journey)
-      console.log('clicked')
     }
     }>
-      <h1>{journey.title}</h1>
-    </button>
-  )
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title={journey.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
+
 
 export default Journey
