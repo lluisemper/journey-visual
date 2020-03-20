7461d3d7eae4967349a7ee8260d7b61bd9b4d1d4
eeden01 <mmvaneeden@gmail.com>
2020-03-20 09:38:57 +0100

    added popup

--- client/src/components/Persona/Persona.js
+++ client/src/components/Persona/Persona.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './Persona.css';
 import personIcon from '../../assets/clipart2682703.png';
+import Popup from "reactjs-popup";
 
 const Persona = ({ persona, setCurrentPersona }) => {
+
+
+
   return (
     <button className='Persona' onClick={() => {
       setCurrentPersona(persona)
@@ -10,6 +14,22 @@ const Persona = ({ persona, setCurrentPersona }) => {
     }>
       <h1>{persona.title}</h1>
       <img className="personIcon" src={personIcon}></img>
+
+
+      <Popup trigger={<button>Trigger</button>} position="top left">
+        {close => (
+          <div className="popUp">
+
+            <h1>{persona.title}</h1>
+
+            <a className="close" onClick={close}>
+              &times;
+        </a>
+          </div>
+        )}
+      </Popup>
+
+
     </button>
   )
 }
