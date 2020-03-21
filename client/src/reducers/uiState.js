950d8daaedbe82b8b89975eb3f03ddeee640da27
lluisemper <lluisemper@gmail.com>
2020-03-21 15:35:18 +0100

    redux thunk

--- client/src/reducers/uiState.js
+++ client/src/reducers/uiState.js
@@ -1,18 +1,29 @@
 const INITIAL_STATE = {
-  steps:[],
-  personas:[],
-  currentPersona:[],
-  
+  steps: [],
+  personas: [],
+  currentPersona: null,
+  journeys: [],
+  currentJourney: null,
 }
+
+
 const uiState = (state = INITIAL_STATE, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case "SET_STEP":
-      return {...state, steps:action.steps}; 
+      return { ...state, steps: action.steps };
     case "SET_PERSONAS":
-      return {...state, personas:action.personas}; 
-    case "SET_CURRENT_PERSONA":
-      return {...state, currentPersona:action.currentPersona}; 
-    default :
+      return { ...state, personas: action.personas };
+    case "SET_JOURNEYS":
+      return { ...state, journeys: action.journeys };
+    case "ADD_JOURNEY":
+      return {
+        ...state,
+        currentJourney: state.currentJourney ? state.currentJourney : action.newJourney,
+        journeys: [...state.journeys, action.newJourney]
+      };
+    case "SET_CURRENT_JOURNEY":
+      return { ...state, currentJourney: action.currentJourney };
+    default:
       return state
   }
 }
